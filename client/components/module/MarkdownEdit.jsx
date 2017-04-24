31e51a666a6d755b5cc94d82ff172e47430b0e9a
xubaoshi <xubaoshi@xubaoshi.com>
2017-04-24 22:00:07 +0800

    add code

--- client/components/module/MarkdownEdit.jsx
+++ client/components/module/MarkdownEdit.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import brace from 'brace';
+import AceEditor from 'react-ace';
+import 'brace/mode/markdown'
+import 'brace/theme/monokai'
+import MarkDownShow from './MarkdownShow'
+
 
 export default class extends React.Component {
+    constructor(props) {
+        super(props)
+        this.editContent = this.content
+    }
+    onChange(newValue) {
+        this.editContent = this.newValue
+    }
     render() {
         const { content } = this.props
+        this.editContent = content
         return (
-            <span id="md">{content}</span>
+            <section>
+                <AceEditor
+                    mode="markdown"
+                    theme="monokai"
+                    name="UNIQUE_ID_OF_DIV"
+                    editorProps={{ $blockScrolling: true }}
+                    value={this.editContent}
+                    onChange={this.onChange}
+                />
+                <MarkDownShow content={this.content} ></MarkDownShow>
+            </section>
         )
     }
 }
